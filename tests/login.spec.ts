8efc9a49891031875158460b1c7c2f035c80b3af
Ken Navarra <knavarra215@gmail.com>
2026-09-10 22:46:07 -0400

    switching to POM

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,20 +1,41 @@
 import { test, expect } from '@playwright/test'
+import { LoginPage } from '../page/login-page';
+import users from '../data/users.json';
 
-test('logs in with valid crednetials', async ({page}) => {
-    await page.goto('https://www.saucedemo.com/');
-    await page.getByPlaceholder('Username').fill('standard_user');
-    await page.getByPlaceholder('Password').fill('secret_sauce');
-    await page.getByRole('button', { name : 'Login'}).click();
+test('logs in via page object', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto()
+    await loginPage.login('standard_user', 'secret_sauce');
     await expect(page).toHaveURL(/inventory/);
-    await expect(page.getByText('Products')).toBeVisible();
 });
 
-test('reject incorrect password', async ({page}) => {
-    await page.goto('https://www.saucedemo.com/');
-    await page.getByPlaceholder('Username').fill('standard_user');
-    await page.getByPlaceholder('Password').fill('incorrect_password');
-    await page.getByRole('button', { name : 'Login'}).click();
-    await expect(page.locator('[data-test="error"]')).toContainText('Username and password do not match');
-    await expect(page).toHaveURL('https://www.saucedemo.com');
-} )
+test('product page loads after login', async ({page}) => {
+    const loginPage= new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.login('standard_user', 'secret_sauce');
 
+    await expect(page.getByText('Products')).toBeVisible();
+    await expect(page.locator('.inventory_item')).toHaveCount(6);
+    await expect(page.getByRole('button', {name: 'Open Menu'})).toBeVisible;
+});
+
+test('waiting for dynamic text', async ({page}) => {
+    await page.goto('https://the-internet.herokuapp.com/dynamic_loading/1');
+    await page.getByRole('button', {name: 'Start'}).click();
+    // Assertion will retry until element appears
+    await expect(page.locator('#finish')).toHaveText('Hello World!');
+});
+
+for (const u of users) {
+  test(`login as ${u.name} user`, async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.login(u.user, u.pass);
+
+    if (u.shouldPass) {
+      await expect(page).toHaveURL(/inventory/);
+    } else {
+      await expect(page.getByText('epic sadface')).toBeVisible();
+    }
+  });
+}
